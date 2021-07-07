2c201fa982c2c7ba24fa603e7c63b0d3ea9608b6
Kelvin Wong <kelvinwong@outlook.com.au>
2021-07-07 17:41:56 +1000

    pass additional xmllint options

--- xml/validate.sh
+++ xml/validate.sh
@@ -3,5 +3,5 @@
 [[ -z $SCHEMA ]] && SCHEMA="deeplink.xsd"
 [[ -z $XML ]] && XML="deeplink.xml"
 
-echo "xmllint --format --schema $SCHEMA $XML"
-xmllint --format --schema $SCHEMA $XML
+echo "xmllint --format --schema $SCHEMA $XML $@"
+xmllint --format --schema $SCHEMA $XML $@
